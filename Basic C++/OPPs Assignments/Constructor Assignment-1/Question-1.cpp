7a7dcf5f7c917fd4c65becbdea736055b08cda36
Ashutosh Raj <107558461+ASHUTOSHRAZZ100@users.noreply.github.com>
2024-09-21 12:57:59 +0000

    solved Basic C++ OPPs Constructor q-1

--- Basic C++/OPPs Assignments/Constructor Assignment-1/Question-1.cpp
+++ Basic C++/OPPs Assignments/Constructor Assignment-1/Question-1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Cuboid
 {
     float length, breadth, height;
+    public:
     Cuboid(float len = 0, float bre = 0, float hei = 0)
     {
         length = len;
@@ -23,11 +24,14 @@ void Cuboid::display()
 int main()
 {
     float len,bre,hig;
-    cout<<"Enter the length";
+    cout<<"Enter the length ";
     cin>>len;
-    cout<<"Enter the breadth";
+    cout<<"Enter the breadth ";
     cin>>bre;
-    cout<<"Enter the height";
+    cout<<"Enter the height ";
     cin>>hig;
+    Cuboid c(len,bre,hig);
+    c.display();
+
     return 0;
 }
